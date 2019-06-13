e1834e671ce7b09ec6d88349df3e828a44f896e1
lining <lining_it@163.com>
2019-06-13 11:25:05 +0800

    T4目录修改

--- src/LnskyDB.Demo/Controllers/ProductSaleByDayController.cs
+++ src/LnskyDB.Demo/Controllers/ProductSaleByDayController.cs
@@ -32,70 +32,60 @@ namespace LnskyDB.Demo.Controllers
 
         // GET http://localhost:53277/ProductSaleByDay/GetList?shopName=1&stTime=2019-01-01&endTime=2019-09-05
         [HttpGet]
-        public ActionResult<List<ProductSaleByDayEntity>> GetList(string shopName, DateTime? stTime, DateTime? endTime)
+        public ActionResult<List<ProductSaleByDayEntity>> GetList()
         {
-            if (stTime == null)
-            {
-                stTime = ProductSaleByDayEntity.MinShuffledTempDate;//最小时间
-            }
-            if (endTime == null)
-            {
-                endTime = DateTime.Now.Date.AddDays(1 - DateTime.Now.Day).AddMonths(1).AddDays(-1);
-            }
-            var repositoryFactory = RepositoryFactory.Create<ProductSaleByDayEntity>();
-            var query = QueryFactory.Create<ProductSaleByDayEntity>(m => m.ShopName.Contains(shopName));
 
+            var stTime = new DateTime(2019, 1, 15);
+            var endTime = new DateTime(2019, 2, 11);
+            var repositoryFactory = RepositoryFactory.Create<ProductSaleByDayEntity>();
+            var query = QueryFactory.Create<ProductSaleByDayEntity>(m => m.ShopName.Contains("测试"));
             query.And(m => m.StatisticalDate >= stTime);
-            query.And(m => m.StatisticalDate < endTime.Value.Date.AddDays(1));
-            // query.DBModel.DBModel_ShuffledTempDate = new DateTime(2019, 01, 01);//这儿表示查19年1月的库和表
+            query.And(m => m.StatisticalDate < endTime.Date.AddDays(1));
+            query.DBModel.DBModel_ShuffledTempDate = new DateTime(2019, 01, 01);//这儿表示查19年1月的库和表
             query.OrderByDescing(m => m.StatisticalDate);
             query.StarSize = 20;
             query.Rows = 10;
             //分库的传入stTime,endTime会自动根据时间查询符合条件的库和表
-            return repositoryFactory.GetList(query, stTime.Value, endTime.Value);
+            var lst = repositoryFactory.GetList(query);
+
+            return lst;
         }
         //GET http://localhost:53277/ProductSaleByDay/GetPaging?shopName=1&stTime=2019-01-01&endTime=2019-03-11&page=2&pageSize=3
         [HttpGet]
-        public ActionResult<object> GetPaging(string shopName, DateTime? stTime, DateTime? endTime, int page = 1, int pageSize = 10)
+        public ActionResult<Paging<ProductSaleByDayEntity>> GetPaging()
         {
 
-            if (stTime == null)
-            {
-                stTime = ProductSaleByDayEntity.MinShuffledTempDate;//最小时间
-            }
-            if (endTime == null)
-            {
-                endTime = DateTime.Now.Date.AddDays(1 - DateTime.Now.Day).AddMonths(1).AddDays(-1);
-            }
+            var stTime = new DateTime(2019, 1, 15);
+            var endTime = new DateTime(2019, 2, 11);
             var repositoryFactory = RepositoryFactory.Create<ProductSaleByDayEntity>();
-            var query = QueryFactory.Create<ProductSaleByDayEntity>(m => m.ShopName.Contains(shopName));
-            query.StarSize = (page - 1) * pageSize;
-            query.Rows = pageSize;
+            var query = QueryFactory.Create<ProductSaleByDayEntity>(m => m.ShopName.Contains("测试"));
             query.And(m => m.StatisticalDate >= stTime);
-            query.And(m => m.StatisticalDate < endTime.Value.Date.AddDays(1));
-            // query.DBModel.DBModel_ShuffledTempDate = new DateTime(2019, 01, 01);//这儿表示差19年1月的库和表
-            //分页查询必须有排序字段
+            query.And(m => m.StatisticalDate < endTime.Date.AddDays(1));
             query.OrderByDescing(m => m.StatisticalDate);
+            query.StarSize = 20;
+            query.Rows = 10;
             //分库的传入stTime,endTime会自动根据时间查询符合条件的库和表
-            var res = repositoryFactory.GetPaging(query, stTime.Value, endTime.Value);
-            return new { res.TotalCount, List = res.ToList() };
+            var paging = repositoryFactory.GetPaging(query, stTime, endTime);
+            var count = paging.TotalCount;
+            var lst = paging.ToList();//或者paging.Items
+            return paging;
         }
 
         // Get http://localhost:53277/ProductSaleByDay/Add?shopName=增加2&StatisticalDate=2019-01-01&dataSource=测试用
         [HttpGet]
-        public void Add(string shopName, DateTime statisticalDate, string dataSource)
+        public void Add()
         {
             var addEntity = new ProductSaleByDayEntity()
             {
                 SysNo = Guid.NewGuid(),
-                DataSource = dataSource,
+                DataSource = "测试来源",
                 ProductID = Guid.NewGuid(),
                 ShopID = Guid.NewGuid(),
-                ShopName = shopName,
-                ProductName = "",
+                ShopName = "测试店铺",
+                ProductName = "测试商品",
                 OutProductID = Guid.NewGuid().ToString(),
                 ImportGroupId = Guid.NewGuid(),
-                StatisticalDate = statisticalDate
+                StatisticalDate = DateTime.Now//分库分表字段是必须的
             };
             var repositoryFactory = RepositoryFactory.Create<ProductSaleByDayEntity>();
             //如果新增主键是自增列会自动赋值自增列值到主键
@@ -103,50 +93,51 @@ namespace LnskyDB.Demo.Controllers
         }
         //http://localhost:53277/ProductSaleByDay/Update?sysNo=650BC09C-2B9C-467B-A457-8B4853CC1F0F&shopName=修改&statisticalDate=2019-01-01&dataSource=修改测试用
         [HttpGet]
-        public bool Update(Guid sysNo, string shopName, DateTime statisticalDate, string dataSource)
+        public bool Update()
         {
             var updateEntity = new ProductSaleByDayEntity()
             {
-                SysNo = sysNo,
-                DataSource = dataSource,
-                ShopName = shopName,
-                StatisticalDate = statisticalDate,//对于分库分表来说StatisticalDate是必须的
-                // DBModel_ShuffledTempDate=statisticalDate,//如果不想更新StatisticalDate字段则用这句话来确定是那个库及表
+                SysNo = Guid.Parse("650BC09C-2B9C-467B-A457-8B4853CC1F0F"),
+                DataSource = "测试来源修改",
+                ShopName = "店铺修改",
+                StatisticalDate = new DateTime(2019, 01, 05),//如果StatisticalDate赋值了则根据StatisticalDate找库表,然后根据主键更新,StatisticalDate也会被更新成所赋的值
+                //如果不想更新StatisticalDate可以用下面这句话
+                // DBModel_ShuffledTempDate=new DateTime(2019,01,05),//如果不想更新StatisticalDate字段则用这句话来确定是那个库及表
             };
             var repositoryFactory = RepositoryFactory.Create<ProductSaleByDayEntity>();
             //根据主键更新其他字段
             return repositoryFactory.Update(updateEntity);
         }
-
+        //http://localhost:53277/ProductSaleByDay/UpdateWhere?shopName=批量修改&shuffledTempDate=2019-02-02&dataSource=更新
+        [HttpGet]
+        public int UpdateWhere()
+        {
+            var updateEntity = new ProductSaleByDayEntity()
+            {
+                DataSource = "测试来源修改",
+                ShopName = "店铺修改Where",
+                DBModel_ShuffledTempDate = new DateTime(2019, 01, 05),//如果用这句话来确定是那个库及表
+                // StatisticalDate = statisticalDate,//如果要更新StatisticalDate则可以用这句话替代上面那句话
+            };
+            var repositoryFactory = RepositoryFactory.Create<ProductSaleByDayEntity>();
+            var where = QueryFactory.Create<ProductSaleByDayEntity>(m => m.ShopName == "测试店铺1" && m.StatisticalDate > new DateTime(2019, 01, 03));//where是更新条件
+            //注意如果是更新用的是实体类的DBModel_ShuffledTempDate Query中的无效
+            return repositoryFactory.Update(updateEntity, where);
+        }
         // http://localhost:53277/ProductSaleByDay/Delete?sysNo=5119AC4F-FA2F-470F-8144-0069B45E15CF&statisticalDate=2019-01-01
         [HttpGet]
-        public bool Delete(Guid sysNo, DateTime statisticalDate)
+        public bool Delete()
         {
 
             var deleteEntity = new ProductSaleByDayEntity()
             {
-                SysNo = sysNo,
-                DBModel_ShuffledTempDate = statisticalDate,//对于分库分表来说DBModel_ShuffledTempDate是必须的用来确认是那个库表
+                SysNo = Guid.Parse("650BC09C-2B9C-467B-A457-8B4853CC1F0F"),
+                DBModel_ShuffledTempDate = new DateTime(2019, 01, 05),//对于分库分表来说DBModel_ShuffledTempDate是必须的用来确认是那个库表
             };
             var repositoryFactory = RepositoryFactory.Create<ProductSaleByDayEntity>();
             return repositoryFactory.Delete(deleteEntity);
         }
-        //http://localhost:53277/ProductSaleByDay/UpdateWhere?shopName=批量修改&shuffledTempDate=2019-02-02&dataSource=更新
-        [HttpGet]
-        public int UpdateWhere(string shopName, DateTime shuffledTempDate, string dataSource)
-        {
-            var updateEntity = new ProductSaleByDayEntity()
-            {
-                DataSource = dataSource,
-                ShopName = shopName,
-                DBModel_ShuffledTempDate = shuffledTempDate,//如果用这句话来确定是那个库及表
-                // StatisticalDate = statisticalDate,//如果要更新StatisticalDate则可以用这句话替代上面那句话
-            };
-            var repositoryFactory = RepositoryFactory.Create<ProductSaleByDayEntity>();
-            var where = QueryFactory.Create<ProductSaleByDayEntity>(m => m.ShopName == "测试店铺1");//where是更新条件
-            //注意如果是更新用的是实体类的DBModel_ShuffledTempDate Query中的无效
-            return repositoryFactory.Update(updateEntity, where);
-        }
+
         //http://localhost:53277/ProductSaleByDay/DleteWhere?shopName=批量修改&shuffledTempDate=2019-02-02&dataSource=新+更
         [HttpGet]
         public int DleteWhere(string shopName, DateTime shuffledTempDate, string dataSource)
@@ -154,12 +145,12 @@ namespace LnskyDB.Demo.Controllers
 
             var repositoryFactory = RepositoryFactory.Create<ProductSaleByDayEntity>();
             var where = QueryFactory.Create<ProductSaleByDayEntity>();
-            where.DBModel.DBModel_ShuffledTempDate = shuffledTempDate;
+            where.DBModel.DBModel_ShuffledTempDate = new DateTime(2019, 01, 01);
             //QueryiSearch方法表示搜索里面空格表示或+表示且
             //如 导入+手工 自动+生成 表示字段必须同时拥有导入和手工或者自动和生成
             //生成sql是 and ((DataSource like '%导入%' and DataSource like '%手工%') or DataSource like '%自动%' and DataSource like '%生成%')            
-            where.QueryiSearch(m => m.DataSource, dataSource);
-            where.QueryiSearch(m => m.ShopName, shopName);
+            where.QueryiSearch(m => m.DataSource, "新+更");
+            where.QueryiSearch(m => m.ShopName, "批量修改");
             //注意如果是更新用的是实体类的DBModel_ShuffledTempDate Query中的无效
             return repositoryFactory.Delete(where);
         }
--- src/LnskyDB.Demo/ThreadTool.cs
+++ src/LnskyDB.Demo/ThreadTool.cs
@@ -6,8 +6,6 @@ namespace Common.Tool
 {
     public class ThreadTool
     {
-
-
         public static void QueueUserWorkItem(Action action)
         {
             ThreadPool.QueueUserWorkItem(delegate
@@ -16,11 +14,6 @@ namespace Common.Tool
                 try
                 {
                     action();
-                }
-                catch (System.Exception ex)
-                {
-                    Console.Error.WriteLine(ex.Message);
-                    Console.Error.WriteLine(ex.StackTrace);
                 }
                 finally
                 {
